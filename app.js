bdf1986c3a30f82f2a6ac7fc3403b7fb4aac4311
John Ovalle <johnovalle@gmail.com>
2017-07-24 03:10:23 +0000

    boiler plate

---
+++ app.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+var CANVAS_WIDTH = 500;
+var CANVAS_HEIGHT = 500;
+
+function update(){
+    ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+}
