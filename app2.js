45fd2dbdd4250c386a8ec37536a51692c6fc2bf7
John Ovalle <johnovalle@gmail.com>
2017-07-25 13:07:18 +0000

    add mouse position debug information and player hit testing

--- app2.js
+++ app2.js
@@ -2,6 +2,10 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 var CANVAS_WIDTH = 500;
 var CANVAS_HEIGHT = 500;
+var mousePosition = {
+    x: 0,
+    y: 0
+}
 
 ctx.font = '30px Arial';
 
@@ -23,8 +27,8 @@ var enemySpdRng = 20;
 var enemySpdMin = 10;
 
 var player = Object.create(Entity);
-player.vx = 30;
-player.vy = 25;
+player.vx = 10;
+player.vy = 15;
 player.type = "player";
 player.text = "P";
 player.id = "player1";
@@ -71,9 +75,13 @@ function update(){
                console.log("enemy " + entity.id, " is hitting player");
            }
        }
+       if(entity.type === "player"){
+           entity.mouseHitting = testPTPCollision(entities["player1"], mousePosition);
+       }
        moveObject(entity);
        drawObject(entity);
    }
+   drawMouseCords();
 }
 
 function moveObject(object){
@@ -90,7 +98,12 @@ function moveObject(object){
     }
 }
 function drawObject(object){
-     ctx.fillText(object.text, object.x, object.y);
+    ctx.save();
+    if(object.mouseHitting){
+        ctx.fillStyle = "#ff0000";
+    }
+    ctx.fillText(object.text, object.x, object.y);
+    ctx.restore();
 }
 
 function getDistanceBetweenPoints(point1, point2){ //point to point
@@ -102,4 +115,19 @@ function getDistanceBetweenPoints(point1, point2){ //point to point
 function testPTPCollision(entity1, entity2){ //point to point
     var distance = getDistanceBetweenPoints(entity1, entity2);
     return distance < 30;
+}
+
+//get mouse position information
+
+document.onmousemove = function(mouse){
+    mousePosition.x = mouse.clientX;
+    mousePosition.y = mouse.clientY;
+};
+
+function drawMouseCords(){
+    var text = "("+mousePosition.x+", "+mousePosition.y+")";
+    ctx.save();
+    ctx.font = "10px Arial";
+    ctx.fillText(text, mousePosition.x, mousePosition.y);
+    ctx.restore();
 }
