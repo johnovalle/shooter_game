74bdc5ff0522784198f5cf48b0207faa841c8b91
John Ovalle <johnovalle@gmail.com>
2017-10-23 18:31:11 -0400

    maintaining some very basic state

--- app.js
+++ app.js
@@ -14,11 +14,17 @@ var game = {
     },
     level: [], // example { enemies: [], powerups: [], stairs: [] },
     screen: {
-        loading: {},
-        start: {},
+        loading: {
+            number: 2
+        },
+        start: {
+            number: 5
+        },
         options: {},
-        play: {},
-        gameOvaer: {}
+        play: {
+            number: 50
+        },
+        gameOver: {}
     }
 }
 
@@ -34,24 +40,63 @@ var Entity = {
     visible: true
 }
 
-var colorPad = {
+game.screen.loading.colorPad = { //cornflower
     r: 30,
     g: 150,
     b: 220
 };
 
+game.screen.start.colorPad = { //lime
+    r: 170,
+    g: 220,
+    b: 120
+};
+
+game.screen.play.colorPad = { //watermelon
+    r: 250,
+    g: 40,
+    b: 70
+};
+
+var  currentState = game.screen.loading;
+
+addEventListener("keydown", (event) => {
+    console.log(event.key);
+    if(event.key === "1"){
+        currentState = game.screen.loading;
+    } else if(event.key === "2") {
+        currentState = game.screen.start;
+    } else if(event.key === "3") {
+        currentState = game.screen.play;
+    }
+
+    if(event.key === "ArrowDown"){
+        currentState.number--;
+    } else if(event.key === "ArrowUp"){
+        currentState.number++;
+    }
+});
+
+
 function update(){
     ctx.clearRect(0, 0, drawingSurface.width, drawingSurface.height);
-    ctx.fillStyle = "rgb(" +colorPad.r + "," + colorPad.g + "," + colorPad.b + ")";
-    colorPad.r += 1;
-    colorPad.g += 2;
-    colorPad.b += 3;
-    for(var color in colorPad){
-        if(colorPad[color] > 255){
-            colorPad[color] -= 255;
-        }
-    }
+    ctx.fillStyle = "rgb(" + currentState.colorPad.r + "," +
+                              currentState.colorPad.g + "," +
+                              currentState.colorPad.b + ")";
+
+
+    //colorPad.r += 1;
+    //colorPad.g += 2;
+    //colorPad.b += 3;
+    //for(var color in colorPad){
+    //    if(colorPad[color] > 255){
+    //        colorPad[color] -= 255;
+    //    }
+    //}
     ctx.fillRect(0, 0, drawingSurface.width, drawingSurface.height);
+    ctx.fillStyle = "#000";
+    ctx.font = "30px Arial";
+    ctx.fillText(currentState.number,10,50);
     requestAnimationFrame(update);
 }
 update();
