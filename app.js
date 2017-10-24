c383e9a447b7db0dbd6452f0326e8ebcb16cbfdc
John Ovalle <johnovalle@gmail.com>
2017-10-24 15:14:46 -0400

    basic turn based action with animation

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ var drawingSurface = {
     height: 500
 };
 
-// different controls on different screens
-// maintain positions and stats for enemies/objects on a level
 // incorperate code to draw tile level and place a guy and have him go from level to level
 // with a start screen
+
+// make turn based
+// create update for non-play related things like background animations etc.
+
 
 var game = {
     state: {
         player: null,
-        current: null
+        current: null,
+        lastMoveFinished: true,
+        playerMoved: false
     },
     level: [], // example { enemies: [], powerups: [], stairs: [] },
     screen: {
@@ -43,7 +47,9 @@ var game = {
             entities: []
         },
         gameOver: {}
-    }
+    },
+    moveAniSpeed: 2,
+
 }
 
 var Entity = {
@@ -56,20 +62,22 @@ var Entity = {
     rotation: 0,
     type: "",
     char: "",
-    visible: true
+    visible: true,
+    nextX: null,
+    nextY: null
 }
 // tell entities how to move themselves
 var monster = Object.create(Entity);
 monster.char = "A";
 monster.x = 50;
 monster.y = 100;
-monster.vx = 1;
+// monster.vx = 1;
 
 var monster2 = Object.create(Entity);
 monster2.char = "B";
 monster2.x = 350;
 monster2.y = 400;
-monster2.vy = -1;
+// monster2.vy = -1;
 
 game.screen.start.entities.push(monster);
 game.screen.start.entities.push(monster2);
@@ -78,7 +86,7 @@ var monster3 = Object.create(Entity);
 monster3.char = "C3";
 monster3.x = 250;
 monster3.y = 30;
-monster3.vy = 1;
+// monster3.vy = 1;
 
 game.screen.play.entities.push(monster3);
 
@@ -106,10 +114,14 @@ game.state.current = game.screen.loading;
 
 function increaseNumber(state){
     state.number++;
+    game.state.playerMoved = true;
+    game.state.lastMoveFinished = false;
 }
 
 function decreaseNumber(state){
     state.number--;
+    game.state.playerMoved = true;
+    game.state.lastMoveFinished = false;
 }
 
 addEventListener("keydown", (event) => {
@@ -129,19 +141,70 @@ addEventListener("keydown", (event) => {
 
 
 function run(){
-    update(game.state.current);
+    if(!game.state.lastMoveFinished){
+        update(game.state.current);
+    }
     draw(game.state.current);
     requestAnimationFrame(run);
 }
 run();
 
+//game.state.playerMoved = true;
+//game.state.lastMoveFinished = false;
+//entity.nextY
+var animationCounter = 0;
 function update(state){
+    // this probably should be in it's own function
+    if(game.state.playerMoved){
+        game.state.playerMoved = false;
+        moveEntities(state);
+    }
+
     for(let i = 0; i < state.entities.length; i++){
         let entity = state.entities[i];
-        entity.x += entity.vx;
-        entity.y += entity.vy;
+        if(entity.x != entity.nextX){
+            entity.x += game.moveAniSpeed * entity.lastMoveX; //change the direction
+        }
+        if(entity.y != entity.nextY){
+            entity.y += game.moveAniSpeed * entity.lastMoveY;
+        }
+    }
+    animationCounter += game.moveAniSpeed;
+    if(animationCounter === 32){
+        animationCounter = 0;
+        game.state.lastMoveFinished = true;
     }
 }
+
+function moveEntities(state){
+    for(let i = 0; i < state.entities.length; i++){
+        let entity = state.entities[i];
+        tempRandomMove(entity);
+        entity.nextX = entity.x + (entity.lastMoveX * 32);
+        entity.nextY = entity.y + (entity.lastMoveY * 32);
+    }
+}
+
+// create some random movements, replace with logic?
+function tempRandomMove(entity){
+    let xMove = 0;
+    let yMove = 0;
+    let randX = Math.random();
+    let randY = Math.random();
+    if(randX < 0.3){
+        xMove = -1;
+    } else if(randX > 0.7){
+        xMove = 1;
+    }
+    if(randY < 0.3){
+        yMove = -1;
+    } else if(randY > 0.7){
+        yMove = 1;
+    }
+    entity.lastMoveX = xMove;
+    entity.lastMoveY = yMove;
+}
+
 
 function draw(state){
     ctx.clearRect(0, 0, drawingSurface.width, drawingSurface.height);
